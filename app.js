67aa8764c6e3269ee5b4f68761422840957aa057
lee-ma <leema9321@gmail.com>
2019-06-18 11:27:46 -0700

    add socket with test message to send over udp protocol

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 'use strict';
 const fs = require('fs');
 const bencode = require('bencode');
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
 
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-console.log(torrent.announce.toString('utf8'));
+const url = urlParse(torrent.announce.toString('utf8'));
+const socket = dgram.createSocket('udp4');
 
+const socketMsg = Buffer.from('test message', 'utf8');
+
+socket.send(socketMsg, 0, socketMsg.length, url.port, url.host, () => {});
+socket.on('message', msg => {
+  console.log(msg);
+});
