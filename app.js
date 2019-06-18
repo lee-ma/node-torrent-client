ff9508087ce0b1e4c1eddab30c1bec313adbf919
lee-ma <leema9321@gmail.com>
2019-06-18 16:07:52 -0700

    cleanup

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 'use strict';
-const fs = require('fs');
-const bencode = require('bencode');
-const dgram = require('dgram');
-const Buffer = require('buffer').Buffer;
-const urlParse = require('url').parse;
 
-const tracker = require('./src/tracker');
 const download = require('./src/download');
 const torrentParser = require('./src/torrent-parser');
 
 const torrent = torrentParser.open(process.argv[2]);
 
-download(torrent);
+download(torrent, torrent.info.name);
