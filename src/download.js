2f7749b0e28d96e726562d7623fb2d430de077a1
lee-ma <leema9321@gmail.com>
2019-06-18 16:06:54 -0700

    add queue class and update download to include all fucntionality

--- src/download.js
+++ src/download.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const fs = require('fs');
 const net = require('net');
 const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
 const message = require('./message');
+
+const Pieces = require('./Pieces');
+const Queue = require('./Queue');
+
+function isJobQueueEmpty(queue) {
+  return queue.length === 0;
+}
 
 function onWholeMsgReceived(socket, callback) {
   let savedBuffer = Buffer.alloc(0);
@@ -22,41 +30,74 @@ function onWholeMsgReceived(socket, callback) {
   });
 }
 
-function requestPiece(socket, requested, queue) {
-  if(requested[queue[0]]) {
-    queue.shift();
-  } else {
-    socket.write(message.buildRequest(pieceIndex));
+function requestPiece(socket, pieces, jobQueue) {
+  if(jobQueue.choked) return null;
+
+  while(jobQueue.length()) {
+    const pieceBlock = jobQueue.deque();
+
+    if(pieces.needed(pieceBlock)) {
+      socket.write(message.buildRequest(pieceBlock));
+      pieces.addRequested(pieceBlock);
+      break;
+    }
   }
 }
 
 function chokeHandler() {
-
+  socket.end();
 }
 
-function unchokeHandler() {
+function unchokeHandler(socket, pieces, jobQueue) {
+  jobQueue.choked = false;
 
+  requestPiece(socket, pieces, jobQueue);
 }
 
-function haveHandler(payload, socket, requested, jobQueue) {
+function haveHandler(socket, pieces, jobQueue, payload) {
   const pieceIndex = payload.readUInt32BE(0);
-  jobQueue.push(pieceIndex);
+  const jobQueueEmpty = isJobQueueEmpty(jobQueue)
+  jobQueue.queue(pieceIndex);
 
-  if(jobQueue.length === 1) {
-    requestPiece(socket, requested, jobQueue);
+  if(jobQueueEmpty) requestPiece(socket, pieces, jobQueue);
+}
+
+function bitfieldHandler(socket, pieces, jobQueue, payload) {
+  const jobQueueEmpty = isJobQueueEmpty(jobQueue);
+  payload.forEach((byte, i) => {
+    for(let j = 0; j < 8; j++) {
+      if(byte % 2) jobQueue.queue(i * 8 + 7 - j);
+      byte = Math.floor(byte/2);
+    }
+  });
+  if(jobQueueEmpty) requestPiece(socket, pieces, jobQueue);
+}
+
+function pieceHandler(socket, pieces, jobQueue, torrent, file, pieceResponse) {
+  pieces.printPercentDone();
+
+  pieces.addReceived(pieceResponse);
+
+  const offset = pieceResponse.index * torrent.info['piece length'] + pieceResponse.begin;
+  fs.write(file, pieceResponse.block, 0, pieceResponse.block.length, offset, () => {});
+
+  if(pieces.isDone()) {
+    console.log("----DONE-----");
+    socket.end();
+    try { 
+      fs.closeSync(file); 
+    } catch(e) { 
+      console.log(e)
+    }
+  } else {
+    requestPiece(socket, pieces, jobQueue);
   }
-}
 
-function bitfieldHandler(payload) {
-
-}
-
-function pieceHandler(payload, socket, requested, jobQueue) {
   jobQueue.shift();
-  requestPiece(socket, requested, jobQueue);
+  requestPiece(socket, pieces, jobQueue);
 }
 
-function msgHandler(msg, socket, requested, jobQueue) {
+function msgHandler(msg, socket, pieces, jobQueue) {
   if(isHandShake(msg)) {
     socket.write(message.buildInterested());
   } else {
@@ -64,19 +105,19 @@ function msgHandler(msg, socket, requested, jobQueue) {
 
     switch(m.id) {
       case 0:
-        chokeHandler();
+        chokeHandler(socket);
         break;
       case 1:
-        unchokeHandler();
+        unchokeHandler(socket, pieces, jobQueue);
         break;
       case 4:
-        haveHandler(m.payload, socket, requested, jobQueue);
+        haveHandler(socket, pieces, jobQueue, m.payload);
         break;
       case 5:
-        bitfieldHandler(m.payload);
+        bitfieldHandler(socket, pieces, jobQueue, m.payload);
         break;
       case 7:
-        pieceHandler(m.payload, socket, requested, jobQueue);
+        pieceHandler(socket, pieces, jobQueue, torrent, file, m.payload);
         break;
       default:
         return;
@@ -85,21 +126,23 @@ function msgHandler(msg, socket, requested, jobQueue) {
 }
 
 function isHandShake(msg) {
-  return msg.length === msg.readUInt8(0) + 49 && msg.toString('utf8', 1) === 'BitTorrent protocol';
+  return msg.length === msg.readUInt8(0) + 49 && msg.toString('utf8', 1, 20) === 'BitTorrent protocol';
 }
 
-function download(peer, torrent, requested) {
+function download(peer, torrent, pieces, file) {
   const socket = net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {
     socket.write(message.buildHandshake(torrent));
   });
-  const jobQueue = [];
-  onWholeMsgReceived(socket, msg => msgHandler(msg, socket, requested, jobQueue));
+  const jobQueue = new Queue(torrent);
+  onWholeMsgReceived(socket, msg => msgHandler(msg, socket, pieces, jobQueue, torrent, file));
 }
 
 module.exports = torrent => {
   tracker.getPeers(torrent, peers => {
-    peers.forEach(download(peer, torrent, requested));
+    const pieces = new Pieces(torrent);
+    const file = fs.openSync(path, 'w');
+    peers.forEach(download(peer, torrent, pieces, file));
   })
 }
--- src/torrent-parser.js
+++ src/torrent-parser.js
@@ -5,6 +5,33 @@ const bencode = require('bencode');
 const crypto = require('crypto');
 const bignum = require('bignum');
 
+module.exports.BLOCK_LEN = Math.pow(2, 14);
+
+module.exports.pieceLen = (torrent, pieceIndex) {
+  const totalLen = bignum.fromBuffer(this.size(torrent)).toNumber();
+  const pieceLen = torrent.info['piece length'];
+
+  const lastPieceLen = totalLen % pieceLen;
+  const lastPieceIndex = Math.floor(totalLen / pieceLen);
+
+  return lastPieceIndex === pieceIndex ? lastPieceIndex : pieceLen;
+}
+
+module.exports.blocksPerPiece = (torrent, pieceIndex) => {
+  const pieceLen = this.pieceLen(torrent, pieceIndex);
+
+  return Math.ceil(pieceLen / this.BLOCK_LEN);
+}
+
+module.exports.blockLen = (torrent, pieceIndex, blockIndex) => {
+  const pieceLen = this.pieceLen(torrent, pieceIndex);
+
+  const lastPieceLen = pieceLen % this.BLOCK_LEN;
+  const lastPieceIndex = Math.floor(pieceLen / this.BLOCK_LEN);
+
+  return blockIndex === lastPieceIndex ? lastPieceLen : this.BLOCK_LEN;
+}
+
 module.exports.open = (filepath) => {
   return bencode.decode(fs.readFileSync(filepath));
 };
