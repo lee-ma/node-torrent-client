a42a8a2fc4869bd46df9e3b9f504b82b523ffa46
lee-ma <leema9321@gmail.com>
2019-06-18 15:11:09 -0700

    add a queue in order to not request pieces more than once

--- src/download.js
+++ src/download.js
@@ -22,6 +22,14 @@ function onWholeMsgReceived(socket, callback) {
   });
 }
 
+function requestPiece(socket, requested, queue) {
+  if(requested[queue[0]]) {
+    queue.shift();
+  } else {
+    socket.write(message.buildRequest(pieceIndex));
+  }
+}
+
 function chokeHandler() {
 
 }
@@ -30,19 +38,25 @@ function unchokeHandler() {
 
 }
 
-function haveHandler(payload) {
+function haveHandler(payload, socket, requested, jobQueue) {
+  const pieceIndex = payload.readUInt32BE(0);
+  jobQueue.push(pieceIndex);
 
+  if(jobQueue.length === 1) {
+    requestPiece(socket, requested, jobQueue);
+  }
 }
 
 function bitfieldHandler(payload) {
 
 }
 
-function pieceHandler(payload) {
-
+function pieceHandler(payload, socket, requested, jobQueue) {
+  jobQueue.shift();
+  requestPiece(socket, requested, jobQueue);
 }
 
-function msgHandler(msg, socket) {
+function msgHandler(msg, socket, requested, jobQueue) {
   if(isHandShake(msg)) {
     socket.write(message.buildInterested());
   } else {
@@ -56,13 +70,13 @@ function msgHandler(msg, socket) {
         unchokeHandler();
         break;
       case 4:
-        haveHandler(m.payload);
+        haveHandler(m.payload, socket, requested, jobQueue);
         break;
       case 5:
         bitfieldHandler(m.payload);
         break;
       case 7:
-        pieceHandler(m.payload);
+        pieceHandler(m.payload, socket, requested, jobQueue);
         break;
       default:
         return;
@@ -74,17 +88,18 @@ function isHandShake(msg) {
   return msg.length === msg.readUInt8(0) + 49 && msg.toString('utf8', 1) === 'BitTorrent protocol';
 }
 
-function download(peer, torrent) {
+function download(peer, torrent, requested) {
   const socket = net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {
     socket.write(message.buildHandshake(torrent));
   });
-  onWholeMsgReceived(socket, msg => msgHandler(msg, socket));
+  const jobQueue = [];
+  onWholeMsgReceived(socket, msg => msgHandler(msg, socket, requested, jobQueue));
 }
 
 module.exports = torrent => {
   tracker.getPeers(torrent, peers => {
-    peers.forEach(download(peer, torrent));
+    peers.forEach(download(peer, torrent, requested));
   })
 }
